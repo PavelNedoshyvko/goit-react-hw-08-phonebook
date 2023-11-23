13fff47a55640b8029f21c7c1d76a82ef05cef57
Pavel Nedoshyvko <plight83@gmail.com>
2023-11-23 23:35:27 +0200

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import { Container, Header, Link, Nav } from './Layout.styled';
+import { Container } from './Layout.styled';
 import { Suspense } from 'react';
 import { HeaderBar } from 'components/AppBar/AppBar';
 
